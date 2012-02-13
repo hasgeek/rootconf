0bd398583e7e76f27ce22289f4d7a6aa0ad6fa57
Nigel Babu <nigelbabu@gmail.com>
2012-02-13 19:01:18 +0530

    Moved to the awesomeness of baseframe

--- rootconf/__init__.py
+++ rootconf/__init__.py
@@ -6,15 +6,23 @@ Website server for rootconf.
 """
 
 from flask import Flask
-from coaster import configureapp
+from flaskext.assets import Environment, Bundle
+from baseframe import baseframe, baseframe_js, baseframe_css
 from os import environ
+from coaster import configureapp
 
 # First, make an app and config it
 
 app = Flask(__name__, instance_relative_config=True)
 configureapp(app, 'ROOTCONF_ENV')
 
+app.register_blueprint(baseframe)
+assets = Environment(app)
+js = Bundle(baseframe_js)
+css = Bundle(baseframe_css)
+assets.register('js_all', js)
+assets.register('css_all', css)
+
 import rootconf.views
 if environ.get('ROOTCONF_ENV') == 'prod':
     import rootconf.loghandler
-
--- rootconf/views/content.py
+++ rootconf/views/content.py
@@ -2,6 +2,7 @@ from rootconf import app
 from flask import render_template
 
 @app.route('/')
+
 def index():
     return render_template('index.html', active=1)
 
