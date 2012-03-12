e9c11205ab139a9efccb1fe5896ada40365d971a
Nigel Babu <nigel@hasgeek.com>
2012-03-12 11:35:24 +0530

    Update content to be more metarefresh style

--- rootconf/views/content.py
+++ rootconf/views/content.py
@@ -2,10 +2,5 @@ from rootconf import app
 from flask import render_template
 
 @app.route('/')
-
 def index():
     return render_template('index.html', active=1)
-
-@app.route('/about')
-def about():
-    return render_template('about.html', active=2)
