5da4e3104b42285de9166c671acd0dc01a6115c9
Nigel Babu <nigel@hasgeek.com>
2012-04-28 12:00:49 +0530

    URL should be /bangalore2012. Fixes #1.

--- rootconf/views/content.py
+++ rootconf/views/content.py
@@ -1,6 +1,10 @@
 from rootconf import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/')
 def index():
+    return redirect(url_for('index2012'), code=302)
+
+@app.route('/bangalore2012')
+def index2012():
     return render_template('index.html', active=1)
