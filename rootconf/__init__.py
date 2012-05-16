3c6479584f7a1e0624f553c0efafb058ef9371df
Nigel Babu <nigel@hasgeek.com>
2012-05-16 19:08:53 +0530

    Screw packing, OSM sucks

--- rootconf/__init__.py
+++ rootconf/__init__.py
@@ -19,9 +19,7 @@ configureapp(app, 'ROOTCONF_ENV')
 app.register_blueprint(baseframe)
 assets = Environment(app)
 js = Bundle(baseframe_js, 'js/leaflet.js')
-css = Bundle(Bundle(baseframe_css, 'css/rootconf.css', 'css/monitor.css',
-                    filters='cssmin', output='css/rootconf-packed.css'),
-                                 'js/leaflet/leaflet.css')
+css = Bundle(baseframe_css, 'css/rootconf.css', 'css/monitor.css', 'css/leaflet.css')
 assets.register('js_all', js)
 assets.register('css_all', css)
 
