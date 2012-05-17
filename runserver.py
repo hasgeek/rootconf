cbd2dd493f2d55dba05b8efce87975cb53fb25e7
Nigel Babu <nigel@hasgeek.com>
2012-05-17 12:24:11 +0530

    Add packing, use a local file instead of placeit

--- runserver.py
+++ runserver.py
@@ -4,5 +4,5 @@ from rootconf import app
 from os import environ
 
 environ['ROOTCONF_ENV'] = 'dev'
-app.config['ASSETS_DEBUG'] = True
+app.config['ASSETS_DEBUG'] = False
 app.run('0.0.0.0', 6200, debug=True)
